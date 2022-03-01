da69d57e36b7032c5a0cd86e9496e22d0f4bd71c
Dylan.Metcalf <dylan.metcalf15@gmail.com>
2022-03-01 23:47:58 +0000

    Updating licenses, question array, saveToFile function, & initialization of app

--- index.js
+++ index.js
@@ -1,135 +1,8 @@
-// TODO: Include packages needed for this application
 // Internal & External modules
 const fs = require("fs");
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
-// TODO: Create an array of questions for user input
-const questions = [
-    // Title
-    {
-        name: "title",
-        message: "What is the title of the project?",
-        type: "input",
-        validate: (input) => {
-            if (!input) {
-                return "Title of project is required";
-            }
-            return true;
-        
-        },
-    },
-        // Project Description
-    {
-        type: "input",
-        name: "description",
-        message: "Describe the project",
-        validate: (input) => {
-            if (!input) {
-                return "Description is required";
-            }
-            return true;
-        },
-    },
-        // Technologies used
-    {
-        type: "checkbox",
-        name: "technologies",
-        message: "Select the technologies used",
-        choices: ["HTML", "CSS", "Javascript", "Node.JS"],
-    },
-    // Install guidelines
-    {
-        type: "input",
-        name: "install",
-        message: "Enter install guidelines",
-        validate: (input) => {
-            if (!input) {
-                return "Provide install guidelines"
-            }
-            return true;
-        },
-    },
-      // usage information
-    {
-        type: "input",
-        name: "usage",
-        message: "Provide usage information",
-        validate: (input) => {
-            if (!input) {
-                return "Usage information required";
-                }
-            return true;
-        },
-    },
-    // contribution
-    {
-        type: "input",
-        name: "contribution",
-        message: "Describe contribution guidelines",
-        validate: (input) => {
-            if (!input) {
-                return "please describe contribution guidelines";
-                }
-            return true;
-        },
-    },
-    // testing guidelines
-    {
-        type: "input",
-        name: "testing",
-        message: "Denote test guidelines",
-        validate: (input) => {
-            if (!input) {
-                return "please provide the test guidelines";
-                }
-            return true;
-        },
-    },
-    // license
-    {
-        type: "list",
-        name: "license",
-        message: "select a license",
-        choices: licenses,
-    },
-  // github username
-    {
-        type: "input",
-        name: "github",
-        message: "Enter Github username",
-        validate: (input) => {
-            if (!input) {
-                return "please enter your github username";
-                }
-            return true;
-        },
-    },
-    // github repository
-    {
-        type: "input",
-        name: "repo",
-        message: "Enter Github repo",
-        validate: (input) => {
-            if (!input) {
-                return "please enter your github repo";
-                }
-            return true;
-        },
-    },
-    // github email
-    {
-        type: "input",
-        name: "email",
-        message: "Enter email address",
-        validate: (input) => {
-            if (!input) {
-                return "please enter a valid email address";
-                }
-        return true;
-        },
-    },
-];
 
 // Licenses choice
 const licenses = [
@@ -186,14 +59,132 @@ const licenses = [
     },
 ];
 
+
+// Questions
+const questions = [
+    // Title
+    {
+        name: "title",
+        message: "What is the title of the project?",
+        type: "input",
+        validate: (input) => {
+            if (!input) {
+                return "Title of project is required";
+            }
+            return true;
+        
+        },
+    },
+        // Project Description
+    {
+        name: "description",
+        message: "Describe the project",
+        type: "input",
+        validate: (input) => {
+            if (!input) {
+                return "Description is required";
+            }
+            return true;
+        },
+    },
+    // Install guidelines
+    {
+        name: "install",
+        message: "Enter install guidelines",
+        type: "input",
+        validate: (input) => {
+            if (!input) {
+                return "Provide install guidelines"
+            }
+            return true;
+        },
+    },
+      // usage information
+    {
+        name: "usage",
+        message: "Provide usage information",
+        type: "input",
+        validate: (input) => {
+            if (!input) {
+                return "Usage information required";
+                }
+            return true;
+        },
+    },
+    // contribution
+    {
+        name: "contribution",
+        message: "Describe contribution guidelines",
+        type: "input",
+        validate: (input) => {
+            if (!input) {
+                return "please describe contribution guidelines";
+                }
+            return true;
+        },
+    },
+    // testing guidelines
+    {
+        name: "testing",
+        message: "Denote test guidelines",
+        type: "input",
+        validate: (input) => {
+            if (!input) {
+                return "please provide the test guidelines";
+                }
+            return true;
+        },
+    },
+    // license
+    {
+        name: "license",
+        message: "select a license",
+        type: "list",
+        choices: licenses,
+    },
+  // github username
+    {
+        name: "github",
+        message: "Enter Github username",
+        type: "input",
+        validate: (input) => {
+            if (!input) {
+                return "please enter your github username";
+                }
+            return true;
+        },
+    },
+    // github repository
+    {
+        name: "repo",
+        message: "Enter Github repo",
+        type: "input",
+        validate: (input) => {
+            if (!input) {
+                return "please enter your github repo";
+                }
+            return true;
+        },
+    },
+    // github email
+    {
+        name: "email",
+        message: "Enter email address",
+        type: "input",
+        validate: (input) => {
+            if (!input) {
+                return "please enter a valid email address";
+                }
+        return true;
+        },
+    },
+];
+
+
 // TODO: Create a function to write README file
 function saveToFile(fileName, data) {
-    fs.writeFile(fileName, generateMarkdown(data), (error) =>
-    error
-        ? console.log("There was an error: " + error)
-        : console.log("Markdown generated")
-    );
-}
+    fs.writeFile(fileName, generateMarkdown(data), (error) => (fileName, generateMarkdown(data))
+)};
 
 // TODO: Create a function to initialize app
 function init() {
